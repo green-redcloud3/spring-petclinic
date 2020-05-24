90b0d278a4ebf18b96ca205b68c08cfb47f582eb
green-redcloud3 <62781092+green-redcloud3@users.noreply.github.com>
2020-05-23 21:29:38 -0700

    this is a comment

--- src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
+++ src/main/java/org/springframework/samples/petclinic/PetClinicApplication.java
@@ -27,6 +27,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication(proxyBeanMethods = false)
 public class PetClinicApplication {
+    // this is a comment
 
 	public static void main(String[] args) {
 		SpringApplication.run(PetClinicApplication.class, args);
